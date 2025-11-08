083b7cd876c8df76729b9c84c16e440d8b6b1c0c
ramisys <fatramcezjames@gmail.com>
2025-11-08 13:28:46 +0800

    Minor fix agi nak katangahan

--- edulog/settings.py
+++ edulog/settings.py
@@ -20,8 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
+load_dotenv()
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
